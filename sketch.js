acdfd38c23fde5849545da74ea3b570cf6e76c5e
Carson <69821062+carson-blythe@users.noreply.github.com>
2021-02-09 13:31:56 -0600

    Updated the sketch file with my sketch file
    Added the part of the sketch file that contains the functions that make the task objects.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+var items = [];
+
 function setup() {
-  createCanvas(400, 400);
+  items.push(new ToiletPaper(50,50));
+  items.push(new Bread(150, 50));
+    items.push(new Masks( 250, 50));
+    items.push(new Sanitizer( 0, 50));
+    items.push(new Camera( 300, 50));
+    items.push(new Coin( 225, 150));
+    items.push(new Puzzle( 225, 250));
+    items.push(new TV( 0, 250));
+    items.push(new Dog( 100, 250));
+    items.push(new Snacks( 150, 300));
+  createCanvas(900, 500);
 }
 
 function draw() {
   background(220);
-}
+    for (var x in items) {
+        items[x].show();
+        
+    }
+    }
