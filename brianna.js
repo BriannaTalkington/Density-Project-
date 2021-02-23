a1d5e12f631387afacd49d8159cb4bdecbcc7a20
BriannaTalkington <77074850+BriannaTalkington@users.noreply.github.com>
2021-02-23 13:53:53 -0600

    Add files via upload

--- brianna.js
+++ brianna.js
@@ -1,1 +1,33 @@
-console.log("hello")
+// Function to put an object on a plate.
+function putOnPlate(item, plate){
+    item.x = plate.x-(item.w/2)
+    item.y= plate.y-(item.h/2)
+}
+
+function lvlGen(n) {
+    var newPlateArr = [];
+    var newItemArr = [];
+    var plateStep= (width/(n+2))
+    console.log(plateStep)
+    var buffer=50
+    
+      // Creating the top plates
+    for(var i=0;i<=n;i++){
+    newPlateArr.push(new Plate(plateStep*(i+1),buffer))
+    }
+    for(var i=0;i<=n;i++){
+    newPlateArr.push(new Plate(plateStep*(i+1),390-buffer))}
+    
+ 
+    /*  newPlateArr.push(new Plate(50, 200));
+    newPlateArr.push(new Plate(200, 50));
+    newPlateArr.push(new Plate(350, 200));
+
+    for (var n in newPlateArr) {
+        newItemArr.push(new Door());
+        putOnPlate(newItemArr[n], newPlateArr[n]);
+    }*/
+    
+    return [newItemArr, newPlateArr];
+}
+
