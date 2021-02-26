5a9e65044345e7e8c5640efbd2b594a73d5e25ad
Bekuo <77074907+Bekuo@users.noreply.github.com>
2021-02-26 13:42:17 -0600

    Updated personal file

--- bekuochukwu.js
+++ bekuochukwu.js
@@ -1,22 +1,25 @@
-
+//plate class and constructor
 class Plate {
 	constructor(x, y){
+		this.name = "Plate";
 		this.x = x;
 		this.y = y;
-		this.diameter = 100;
+		this.w = 100;
+		this.h = 100;
 		this.r = 91;
 		this.g = 199;
 		this.b = 225;
 	}		
 	show(){
 		fill(this.r, this.g, this.b);
-		circle(this.x, this.y, this.diameter);
+		rect(this.x, this.y, this.w, this.h, this.w/2);
 	}
 }
 
-
+//door class and constructor
 class Door {
 	constructor(x, y, w, h){
+		this.name = "Door";
 		this.x = x;
 		this.y = y;
 		this.r = 91;
@@ -28,5 +31,29 @@ class Door {
 	show(){
 		fill(this.r, this.g, this.b);
 		rect(this.x, this.y, this.w, this.h);
+	}
+}
+
+// Returns the distance between two points
+function objDist(obj1, obj2){	
+	
+	var x1 = obj1.x+(obj1.w / 2);
+	var y1 = obj1.y+(obj1.h / 2);
+	
+	var x2 = obj2.x+(obj2.w / 2);
+	var y2 = obj2.y+(obj2.h / 2);
+	
+//run of ¨slope¨
+		var a = x2 - x1;
+//rise of ¨slope¨
+		var b = y2 - y1;
+//distace form
+		var c = Math.sqrt( (a*a) + (b*b) );
+	if (keyIsPressed === true) {
+		if (obj1.name && obj2.name) {
+			console.log("The distance from "+obj1.name+" to "+
+				obj2.name+" is "+c+".");	
+	}
+	return (c);
 	}
 }
