7a2160049342358e8ed8472899ac879867bee7d4
Gaile2023 <77074873+Gaile2023@users.noreply.github.com>
2021-03-24 14:03:07 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,8 @@ var yOffset;
 var charSetup;
 var taskBar;
 var info = [];
-var doors = [];//
+var doors = [];
+var distNumb = 50
 //var answers = [];
 
 
@@ -132,11 +133,30 @@ function keyPressed() {
 	if (keyIsDown(DOWN_ARROW)) {
 	charSetup.moveDown()  
  	}
-    if (keyIsDown(32)) {
-	setDensities()  
+    if (keyIsDown(32)) { //SPACEBAR
+	//setDensities()  
  	}
+	
+		for(var i = 0; i < items.length; i++) {
+			console.log(items[i]);
+			if (objDist(charSetup, items[i]) < distNumb) {
+				putOnPlate(items[i],plates[i])
+			}
+		}
 
-
+		if (charSetup.hold != -1) {
+			charSetup.carry(items[charSetup.hold]);
+		}
+	
+	/*
+	if (keyIsDown(ENTER)) {
+	charSetup.hold = 0
+	} 
+	*/
+	
+displayDensities(densities)
+    
+    
     }
 
 
